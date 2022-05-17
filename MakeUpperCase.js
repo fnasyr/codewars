385ad1938a2ee1dbee5c9c846874620041122dc5
fnasyr <fnasyr000@gmail.com>
2022-05-16 23:12:36 -0400

    added solution

---
+++ MakeUpperCase.js
@@ -0,0 +1,16 @@
+// Write a function which converts the input string to uppercase.
+
+
+// Solution:
+
+//Parameter: string
+//Result: return
+// Pseudo:
+//converts the input string to upperCase
+      
+function makeUpperCase(str) {
+    return str.toLocaleUpperCase();
+  }
+
+  //result:
+ makeUpperCase('hello')//HELLO
