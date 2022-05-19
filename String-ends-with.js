05d97d9100b8992820184343efc1f9edfefc37ad
fnasyr <fnasyr000@gmail.com>
2022-05-19 15:15:12 -0400

    added solution

---
+++ String-ends-with.js
@@ -0,0 +1,20 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false 
+
+
+
+
+
+
+
+function solution(str, ending){
+    if (ending.length < 1){
+      return true;
+    }
+    return str.slice(-ending.length,str.length) == ending
+  }
+
