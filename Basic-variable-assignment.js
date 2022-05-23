515803c3bbe55c9e26451acd68df4690c320efbd
fnasyr <fnasyr000@gmail.com>
2022-05-23 14:49:58 -0400

    added solution

---
+++ Basic-variable-assignment.js
@@ -0,0 +1,11 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+
+
+const a = "code";
+const b = "wa.rs";
+const name = `${a}${b}`;
+
+
+
