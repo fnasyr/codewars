8a0adb7bb13dc3d2bba2248e2f9e49a7acb5a08b
fnasyr <fnasyr000@gmail.com>
2022-05-25 12:51:09 -0400

    added solution

---
+++ Square-n-Sum.js
@@ -0,0 +1,23 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+//SOLUTION:
+
+
+//Parameter: number
+//Result: Console log
+//Pseudo: Do the square for each number, and return the sum of all these square numbers.
+
+
+function squareSum(numbers) {
+    let sum = 0
+    for( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] **2
+    }
+    return sum
+}
+
+
+
