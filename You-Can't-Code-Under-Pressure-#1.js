764d257694a45002cc978db15dd45390764a6c52
fnasyr <fnasyr000@gmail.com>
2022-06-02 23:51:46 -0400

    addded solution

---
+++ You-Can't-Code-Under-Pressure-#1.js
@@ -0,0 +1,19 @@
+// Code as fast as you can! You need to double the integer and return it. 
+
+
+
+// Solution:
+//Parameter: i is integer (whole number)
+//Result: console
+//Pseudo: multiply i by two and return the result 
+
+
+
+	function doubleInteger(i) {
+		double = i*2
+		return double;
+	  }
+
+//result: 
+//console.log(2)//4
+//console.log(-10)//-20
