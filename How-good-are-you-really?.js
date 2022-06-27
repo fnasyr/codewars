6be75426ca1a776c25ab21b07522568661fd36ca
fnasyr <fnasyr000@gmail.com>
2022-06-26 21:57:44 -0400

    added solution

---
+++ How-good-are-you-really?.js
@@ -0,0 +1,27 @@
+
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+
+
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    var classAvg = 0;
+    for (var i = 0; i < classPoints.length; i++){
+      classAvg += classPoints[i]; 
+    }
+    classAvg = classAvg/classPoints.length; 
+    return yourPoints > classAvg;
+  }
+
